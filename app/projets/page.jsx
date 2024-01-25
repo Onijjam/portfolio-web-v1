69baee9c8e65ca37d87b1d99ae91e0471eae0307
gautm <gaut.montagne@gmail.com>
2024-01-25 17:16:06 +0100

    Fix: ajustement pour le build

--- app/projets/page.jsx
+++ app/projets/page.jsx
@@ -1,7 +1,7 @@
 'use client';
 import React, {useState} from "react";
-import {Tabs, TabsList, TabsContent, TabsTrigger} from "@/components/ui/tabs"
-import ProjectCard from "@/components/ProjectCard";
+import {Tabs, TabsList, TabsContent, TabsTrigger} from "/components/ui/tabs"
+import ProjectCard from "/components/ProjectCard";
 
 const projectData = [
     {
